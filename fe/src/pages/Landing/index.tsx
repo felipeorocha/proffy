d69310a94c0d3996e9f9da1c7e6fcff9be3fe5b9
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-08-12 02:28:05 -0300

    Router routes added

--- fe/src/pages/Landing/index.tsx
+++ fe/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -22,14 +23,14 @@ function Landing() {
           className="hero-image"
         />
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Study"/>
             Estudar
-          </a>
-          <a href="" className="give-classes">
+          </Link>
+          <Link to="/give-class" className="give-classes">
             <img src={giveClassesIcon} alt="Class"/>
             Dar aula
-          </a>
+          </Link>
         </div>
         <span className="total-connections">
           Total de 200 conexoes realizadas
