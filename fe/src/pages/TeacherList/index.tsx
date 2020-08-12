4ccab2e4933ab1bd1ebd469985ce4bdefeb040da
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-08-12 02:49:08 -0300

    PageHeader component

--- fe/src/pages/TeacherList/index.tsx
+++ fe/src/pages/TeacherList/index.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import logoImg from '../../assets/images/logo.svg';
-import backIcon from '../../assets/images/icons/back.svg';
+import PageHeader from '../../components/PageHeader';
 
 import './styles.css'
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="className">
-      <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Back"/>
-          </Link>
-          <img src={logoImg} alt="Proffy"/>
-        </div>
-        <div className="header-content">
-          <strong>Esses sao os proffys disponiveis.</strong>
-        </div>
-      </header>
+      <PageHeader />
     </div>
   )
 }
