534c0ef1dd57a985d09ac0d1009cd72b0ce7d77b
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-08-12 02:43:30 -0300

    Teacher list route and styles

--- fe/src/pages/TeacherList/index.tsx
+++ fe/src/pages/TeacherList/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+
+import './styles.css'
 
 function TeacherList() {
   return (
-    <h1>Teachers list</h1>
+    <div id="page-teacher-list" className="className">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Back"/>
+          </Link>
+          <img src={logoImg} alt="Proffy"/>
+        </div>
+        <div className="header-content">
+          <strong>Esses sao os proffys disponiveis.</strong>
+        </div>
+      </header>
+    </div>
   )
 }
 
